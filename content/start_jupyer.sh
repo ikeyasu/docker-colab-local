2d7130f4314dbf3e141ae79f742b09be50cc6ec3
ikeyasu <ikeyasu@gmail.com>
2018-04-02 10:59:30 +0900

    Remove LD_LICARY_PATH setting fou cuda.

--- content/start_jupyer.sh
+++ content/start_jupyer.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-# set cuda library path
-export LD_LIBRARY_PATH=/usr/local/cuda-9.0/targets/x86_64-linux/lib:$LD_LIBRARY_PATH
-
 # Enable http_over_ws (It was enabled on Dockerfile but it is neccesarry becauseof unknown reason)
 jupyter serverextension enable --py jupyter_http_over_ws
 
